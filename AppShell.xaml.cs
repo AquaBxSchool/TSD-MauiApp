76a7957d095e2bafdb94e0bfa3ea4327370805e4
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-13 22:01:55 +0200

    init

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TSD_MauiApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TSD_MauiApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
